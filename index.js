163bb39ec3e4eb107d120932f2de1633767418df
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-12-06 18:13:12 -0600

    Custom middleware that logs request details

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ app.use(morgan('combined'));
 //Helmet Middleware
 app.use(helmet());
 
+//Custom middleware that logs request details
+app.use((req, res, next) => {
+    req.time = new Date(Date.now()).toString(); //Current time
+    console.log(req.method, req.hostname, req.path, req.time); //Output: GET localhost / Thu Dec 05 2024 21:24:24 GMT-0600 (Central Standard Time) 'method hostname path time'
+    next();
+});
+
 
 
 //Basic route
